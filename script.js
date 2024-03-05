e6ed00fe54a411d106190472f16c1e9549445073
siyovush-hamidov <siyovushchik14@gmail.com>
2024-03-05 15:24:37 +0500

    Created index.html, got ip from js

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
+fetch('https://ipapi.co/json/')
+  .then(d => d.json())
+  .then(d => document.querySelector('#ip').innerHTML = d.ip);
 
-// Add your custom JavaScript logic here
-document.getElementById("calculate-btn").addEventListener("click", function() {
-    const ipAddress = document.getElementById("ip-address").value;
-    const subnetMask = document.getElementById("subnet-mask").value;
-
-    // Perform IP address calculations (not implemented in this example)
-    // Replace the following lines with your actual logic
-    const classInfo = "Class A";
-    const networkRange = "10.0.0.1 - 10.255.255.254";
-    const networkNumber = "10.0.0.0";
-    const hostNumber = "1";
-
-    // Display results
-    document.getElementById("results").innerHTML = `
-        <p>IP Address: ${ipAddress}</p>
-        <p>Subnet Mask: ${subnetMask}</p>
-        <p>Class: ${classInfo}</p>
-        <p>Network Range: ${networkRange}</p>
-        <p>Network Number: ${networkNumber}</p>
-        <p>Host Number: ${hostNumber}</p>
-    `;
-});
